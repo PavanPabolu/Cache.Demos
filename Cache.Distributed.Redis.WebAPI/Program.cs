4e73827731592438793fb7509a6b4d7db0eaeffc
PavanPabolu <PavanPabolu.edu@gmail.com>
2025-01-21 00:18:12 +0530

    Microsoft.Extensions.Caching.StackExchangeRedis

--- Cache.Distributed.Redis.WebAPI/Program.cs
+++ Cache.Distributed.Redis.WebAPI/Program.cs
@@ -3,6 +3,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+//To read data from Database
+//builder.Services.AddDbContext<AdventureWorksDbContext>(options =>
+//        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+//Distributed Caching - Setting Up Redis Server On Windows
+//https://github.com/microsoftarchive/redis/releases/tag/win-3.0.504
+
+//PM> install-package Microsoft.Extensions.Caching.StackExchangeRedis
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration["RedisCacheServerUrl"];
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
